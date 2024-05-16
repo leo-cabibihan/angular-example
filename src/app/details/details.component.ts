98ec77bfda3f01f17cd7eed9b471969b39f3bb3a
leo-cabibihan <leocabibihan@outlook.com>
2024-05-16 16:49:15 +0800

    added form validation

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {last} from "rxjs";
 
 @Component({
   selector: 'app-details',
@@ -12,10 +13,10 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
   template: `
     <article>
       <img
-        class="listing-photo"
-        [src]="housingLocation?.photo"
-        alt="Exterior photo of {{ housingLocation?.name }}"
-        crossorigin
+          class="listing-photo"
+          [src]="housingLocation?.photo"
+          alt="Exterior photo of {{ housingLocation?.name }}"
+          crossorigin
       />
       <section class="listing-description">
         <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
@@ -31,16 +32,45 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to live here</h2>
-        <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <form [formGroup]="applyForm!!" (submit)="submitApplication()">
           <label for="first-name">First Name</label>
-          <input id="first-name" type="text" formControlName="firstName" />
+          <input id="first-name" type="text" formControlName="firstName" required/>
+
+          <div *ngIf="firstName?.invalid && (firstName?.dirty || firstName?.touched)"
+               class="alert alert-danger">
+                <div *ngIf="firstName?.hasError('required')">
+                  FirstName is required.
+                </div>
+          </div>
+          
           <label for="last-name">Last Name</label>
-          <input id="last-name" type="text" formControlName="lastName" />
+          <input id="last-name" type="text" formControlName="lastName"/>
+
+          <div *ngIf="lastName?.invalid && (lastName?.dirty || lastName?.touched)"
+               class="alert alert-danger">
+            <div *ngIf="lastName?.hasError('required')">
+              LastName is required.
+            </div>
+
+          </div>
+          
           <label for="email">Email</label>
-          <input id="email" type="email" formControlName="email" />
+          <input id="email" type="email" formControlName="email"/>
+
+          <div *ngIf="email?.invalid && (email?.dirty || email?.touched)"
+               class="alert alert-danger">
+            <div *ngIf="email?.hasError('required')">
+              Email is required.
+            </div>
+            <div *ngIf="email?.hasError('pattern')">
+              Has to follow email format JohnDoe@company.com
+            </div>
+          </div>
           <button type="submit" class="primary">Apply now</button>
         </form>
+
       </section>
+    
     </article>
   `,
   styleUrls: ['./details.component.css']
@@ -50,22 +80,44 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
   
-  applyForm= new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-  });
-  
-  submitApplication() {
-    this.housingService.submitApplication(
-      this.applyForm.value.firstName ?? '',
-      this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? '',
-    );
+  applyForm: FormGroup | undefined
+
+  ngOnInit(): void{
+    this.applyForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    });
+
   }
-  
+
+  get firstName() {
+    return this.applyForm?.get('firstName');
+  }
+
+  get lastName() {
+    return this.applyForm?.get('lastName');
+  }
+  get email() {
+    return this.applyForm?.get('email');
+  }
+
+  submitApplication() {
+    if(this.applyForm?.invalid) return
+    this.housingService.submitApplication(
+      this.applyForm?.value.firstName ?? '',
+      this.applyForm?.value.lastName ?? '',
+      this.applyForm?.value.email ?? '',
+    );
+
+  }
+
+
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {this.housingLocation = housingLocation});
   }
-} 
+
+  protected readonly last = last;
+}
